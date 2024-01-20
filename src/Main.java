460499a62b9f37f15f619761fe5c3c010d4df7f6
alextentsov <156477651+alextentsov@users.noreply.github.com>
2024-01-20 14:42:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет!");
-
+        System.out.println("Калькулятор");
+        System.out.println("Результат перемножения чисел:" + sum);
     }
 }
