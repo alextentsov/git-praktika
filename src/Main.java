1210bdce43a8b5a175daf9c1b17c57781320c061
Alexandr Tentsov <tentsow@yandex.ru>
2024-01-23 01:23:49 +0300

    removed duplicate code

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,21 @@ public class Main {
         double div;
         double sub;
         double a = 2;
-        double b = 3;
+        double b = 0;
 
         mul = a * b;
         sum = a + b;
-        div = (a / b);
+        div = a / b;
         sub = a - b;
 
-        System.out.println("Калькулятор");
-        System.out.println("Результат перемножения чисел:" + mul);
         System.out.println("Результат сложения чисел:" + sum);
-        System.out.println("Результат деления чисел:" + div);
         System.out.println("Результат вычитания чисел:" + sub);
+        System.out.println("Результат перемножения чисел:" + mul);
+
+        if (b == 0) {
+            System.out.println("Деление на 0");
+        } else {
+            System.out.println("Результат деления чисел:" + div);
+        }
     }
 }
