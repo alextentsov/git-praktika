689d4f0bb2eff309463411d33cf01ba0a6b95895
Alexandr Tentsov <tentsow@yandex.ru>
2024-01-24 23:33:25 +0300

    added text for scanners

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,10 @@ public class Main {
         double div;
         double sub;
 
+        System.out.println("Введите первое число: ");
         double a = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число: ");
         double b = new Scanner(System.in).nextInt();
 
         sum = a + b;
@@ -17,14 +20,14 @@ public class Main {
         div = a / b;
 
         System.out.println("Калькулятор\n");
-        System.out.println("Результат сложения чисел:" + sum);
-        System.out.println("Результат вычитания чисел:" + sub);
-        System.out.println("Результат перемножения чисел:" + mul);
+        System.out.println("Результат сложения чисел: " + sum);
+        System.out.println("Результат вычитания чисел: " + sub);
+        System.out.println("Результат перемножения чисел: " + mul);
 
         if (b == 0) {
             System.out.println("Деление на 0");
         } else {
-            System.out.println("Результат деления чисел:" + div);
+            System.out.println("Результат деления чисел: " + div);
         }
     }
 }
